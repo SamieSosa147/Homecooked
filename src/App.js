153ac2de1fba32295bd0ef34fd071b732016f600
kchia <kchia87@gmail.com>
2020-08-12 21:41:31 -0400

    add feedback

--- src/App.js
+++ src/App.js
@@ -14,48 +14,50 @@ class App extends React.Component {
 		};
 	}
 	getData = (search) => {
+
 		let Id = '38f30bc0';
+		// Hou comment: please store your api key in a .env file. See https://git.generalassemb.ly/seir-622/project-2/blob/master/project-setup-and-deployment-guide.md#storing-api-keys-outside-of-github for more details
 		let key = '0c4c2cf770237683afd478fb2c8313a7';
+
+		// Hou comment: as a follow up challenge, read about the async/await pattern and try to refactor
+		// your fetch call to use that pattern: https://dev.to/shoupn/javascript-fetch-api-and-using-asyncawait-47mp
 		fetch(
 			`https://api.edamam.com/search?q=${search}&app_id=${Id}&app_key=${key}`
 		)
 			.then((res) => res.json())
 			.then((json) => {
 				this.setState({ data: json.hits });
+				// Hou comment: make sure to delete any unused code from your files, like line 26 below
 				// console.log(this.state.data);
 			});
 	};
 	handleSubmit = (event) => {
 		event.preventDefault();
 		this.getData(this.state.search);
-		// this.setState({ search: event.target.value });
-
-		// alert("hello")
 	};
-	handleChange = (e) => {
-		// console.log(e.target.value);
-		this.setState({ search: e.target.value });
+
+	// Hou comment: use `event` as the variable name instead of `e`, because the former is
+	// more descriptive
+	handleChange = (event) => {
+		this.setState({ search: event.target.value });
 	};
 
 	render() {
 		return (
 			<section>
-				<div>
-					<form className='form' onSubmit={this.handleSubmit}>
-						<h1>Home-Cooked</h1>
-						<input
-							className=' search-box'
-							type='text'
-							onChange={this.handleChange}
-							value={this.state.search}
-						/>
-						<button className='Submit-button' type='submit'>
-							Munch-Time!
-						</button>
-					</form>
-
-					<Home data={this.state.data} />
-				</div>
+				<form className='form' onSubmit={this.handleSubmit}>
+					<h1>Home-Cooked</h1>
+					<input
+						className=' search-box'
+						type='text'
+						onChange={this.handleChange}
+						value={this.state.search}
+					/>
+					<button className='Submit-button' type='submit'>
+						Munch-Time!
+					</button>
+				</form>
+				<Home data={this.state.data} />
 			</section>
 		);
 	}
--- src/Results.js
+++ src/Results.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 
+// Hou comment: As a follow-up challenge, how would you refactor Results to use a functional component + Hooks? In general, how would you refactor your class components that don't rely state to use functional components?
 class Results extends React.Component {
 	render() {
+		// Hou comment: remove any console.log's from your code
 		console.log(this.props.ingredients);
 		return (
 			<section className='Results'>
@@ -11,14 +13,13 @@ class Results extends React.Component {
 					<Link to={`home/${this.props.title}/`}>
 						<h1>{this.props.title}</h1>
 					</Link>
-
+					{/* Hou comment: it's considered an SEO best practice to include an alt attribute to describe your images */}
 					<img src={this.props.image} />
 					<ul>
 						{this.props.ingredients.map((ingredient) => {
 							return <li>{ingredient.text}</li>;
 						})}
 					</ul>
-					<div></div>
 				</div>
 			</section>
 		);
